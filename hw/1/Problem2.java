72498b7c1152583ec1f504176a8963a8f96f3fd4
Scott Scherzer <scottscherzer@Scotts-MacBook-Air.local>
2020-09-04 17:23:35 -0400

    assignment1

---
+++ hw/1/Problem2.java
@@ -0,0 +1,22 @@
+package problem1.java;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		// a = (v1- v0) / t
+		double v0 = 5.6;
+		double v1 = 10.5;
+		double t = 0.5;
+		
+		double answer = (v1- v0) / t;
+		System.out.println("answer = " + answer);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
